22717bce68a09be086807506d0cae6c47a6f1961
vbcpascal <vbcpascal@outlook.com>
2022-01-21 14:17:00 +0800

    Remove from_basic

--- src/ssa.rs
+++ src/ssa.rs
@@ -9,13 +9,13 @@ use depile::analysis::control_flow::{BranchingBehaviour, HasBranchingBehaviour};
 use parse_display::{Display, FromStr};
 use depile::ir::instr::basic::{Branching, Marker, InterProc, Operand};
 use depile::ir::instr::InstrExt;
-use crate::ssa::from_basic::func_from_basic;
 
 /// Instruction kind SSA
 pub type SSAKind = depile::ir::instr::Kind<SSAOpd, Branching, Marker, InterProc, Phi>;
 
 /// SSA block.
 pub type Block = depile::ir::Block<SSAKind>;
+pub type Blocks = depile::ir::Blocks<SSAKind>;
 pub type Function = depile::ir::Function<SSAKind>;
 pub type Functions = depile::ir::Functions<SSAKind>;
 
@@ -67,47 +67,11 @@ impl HasBranchingBehaviour for Phi {
     }
 }
 
-pub fn ir_from_basic<K: InstrExt>(funcs: &depile::ir::Functions<K>) -> Functions {
-    Functions {
-        functions: funcs.functions.iter().map(|x| func_from_basic(x)).collect(),
-        entry_function: funcs.entry_function,
-    }
-}
-
-mod from_basic {
-    use depile::ir::instr::{InstrExt, Instr};
-    use depile::ir::instr::Instr::Binary;
-    use super::{Block, SSAInstr, Function};
-
-    pub fn func_from_basic<K: InstrExt>(func: &depile::ir::Function<K>) -> Function {
-        Function {
-            parameter_count: func.parameter_count,
-            local_var_count: func.local_var_count,
-            entry_block: func.entry_block,
-            blocks: func.blocks.iter().map(|x| block_from_basic(x)).collect(),
-        }
-    }
-
-    pub fn block_from_basic<K: InstrExt>(block: &depile::ir::Block<K>) -> Block {
-        Block {
-            first_index: block.first_index,
-            instructions: block.instructions.iter().map(|i| instr_from_basic(i)).collect(),
-        }
-    }
-
-    pub fn instr_from_basic<K: InstrExt>(instr: &depile::ir::Instr<K>) -> SSAInstr {
-        todo!()
-    }
-
-}
-
-
-
-
 #[cfg(test)]
 mod tests {
     use depile::ir::Blocks;
-    use super::{SSAInstr, Phi, SSAOpd, ir_from_basic};
+    use crate::ssa::SSAKind;
+    use super::{SSAInstr, Phi, SSAOpd};
 
     macro_rules! assert_equiv {
         ($($str: expr => $val: expr),+ $(,)?) => {
@@ -138,16 +102,5 @@ mod tests {
                     SSAOpd::Subscribed("i".to_string(), 1),
                 ])}),
         }
-    }
-
-    #[test]
-    fn test_read() {
-        use depile::ir::program::read_program;
-        use crate::samples;
-
-        let program = read_program(samples::GCD).unwrap();
-        let blocks = Blocks::try_from(program.as_ref()).unwrap();
-        let functions = ir_from_basic(&blocks.functions().unwrap());
-        println!("{}", functions);
     }
 }
